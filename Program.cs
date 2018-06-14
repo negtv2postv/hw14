d3099cb165abe4821e0e22e03b645719c27c056e
negtv2postv <39930370+negtv2postv@users.noreply.github.com>
2018-06-14 15:34:25 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HW14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string food = "Mexican Dish";
+            Console.WriteLine(food);
+            Console.ReadKey();
+
+            int quantity = 1;
+            Console.WriteLine(quantity);
+            Console.ReadKey();
+
+            float size;
+            size = 1.5f;
+            Console.WriteLine(size);
+            Console.ReadKey();
+
+            double seconds;
+            seconds = 2;
+            Console.WriteLine(seconds);
+            Console.ReadKey();
+
+            bool authentic = true;
+            Console.WriteLine(authentic);
+            Console.ReadKey();
+
+            string taste = "deliscious";
+            Console.WriteLine(taste);
+            Console.ReadKey();
+
+            Console.WriteLine("This {0} of {1} serving is {2}", food, size, taste);
+            Console.ReadKey();
+
+            Console.Write("What is your fav mexican dish: ");
+            string dishName = Console.ReadLine();
+            Console.WriteLine(dishName);
+            Console.ReadKey();
+
+            Console.WriteLine("Puerto Riccan food" == "Mexican food");
+            Console.WriteLine("I love != tacos");
+            Console.WriteLine("One taco" ==  "two tacos");
+            Console.WriteLine("one taco" != "one tacos");
+            Console.ReadKey();
+        }
+    }
+}
